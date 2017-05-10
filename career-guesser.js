87c3b81187d75803882e8d47f41a162d7f37714b
Bæring Gunnar Steinþórsson <baering90@gmail.com>
2017-05-10 08:27:11 +0000

    Refactor career-guesser.js

--- career-guesser.js
+++ career-guesser.js
@@ -1,210 +1,31 @@
 import { cleanString } from './utils'
+import * as careerWords from './career-words'
 
-function isAthlete(wordsInCareer) {
-  return wordsInCareer.indexOf('atvinnumaður') !== -1
-}
-
-function isManualWorker(wordsInCareer) {
-  if (wordsInCareer.indexOf('verka') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('bóndi') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('gjaldkeri') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('virki') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('skógarvörður') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('sölufulltrúi') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('húsmóðir') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('leikkona') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('bókari') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('smiður') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('maður') !== -1) {
-    // sjómaður, starfsmaður, verðgæslumaður, verslunarmaður
-    return true
-  } else if (wordsInCareer.indexOf('lögreglu') !== -1) {
-    // lögreglufulltrúi, lögregluþjónn
-    return true
-  } else if (wordsInCareer.indexOf('matsveinn') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('vörður') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('liði') !== -1) {
-    // sjúkraliði
-    return true
-  } else if (wordsInCareer.indexOf('meistari') !== -1) {
-    // sjúkraliði
-    return true
-  } else if (wordsInCareer.indexOf('sali') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('eftirlaunaþegi') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('stuðningsfulltrúi') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('tæknir') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('prentari') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('teiknari') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('leikari') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('þjálfari') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('iðnrekandi') !== -1) {
-    return true
-  }
-
-  return false
-}
-
-function isJournalist(wordsInCareer) {
-  if (wordsInCareer.indexOf('blaðamaður') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('fréttamaður') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('þáttastjórnandi') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('útgefandi') !== -1) {
-    return true
-  }
-}
-
-function isTeacher(wordsInCareer) {
-  if (wordsInCareer.indexOf('kennari') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('kenndi') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('dósent') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('lektor') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('prófessor') !== -1) {
-    return true
-  }
-  return false
-}
-
-function isBarrister(wordsInCareer) {
-  if (wordsInCareer.indexOf('lögmaður') !== -1) {
-    return true
-  }
-  return false
-}
-
-function isWhiteCollar(wordsInCareer) {
-  if (wordsInCareer.indexOf('fræðingur') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('læknir') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('nemi') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('prestur') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('ráðgjafi') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('sjúkraþjálfari') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('rithöfundur') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('eftirlitsmaður') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('þjálfi') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('endurskoðandi') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('sérfræðingur') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('forstöðu') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('hjúkrun') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('heimspekingur') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('arkitekt') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('fulltrúi') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('prófastur') !== -1) {
-    return true
-  }
-
-  return false
-}
-
-function isCivilService(wordsInCareer) {
-  if (wordsInCareer.indexOf('skrifstofu') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('formaður') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('borgarfulltrúi') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('bæjarfulltrúi') !== -1) {
-    return true
-  }
-  return false
-}
-
-function isPolititian(wordsInCareer) {
-  if (wordsInCareer.indexOf('fógeti') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('sýslumaður') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('bæjarstjóri') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('borgarstjóri') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('aðstoðarmaður') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('forseti') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('oddviti') !== -1) {
-    return true
-  }
-  return false
-}
-
-function isExecutive(wordsInCareer) {
-  if (wordsInCareer.indexOf('stjór') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('stýra') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('kaupmaður') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('útgerðarmaður') !== -1) {
-    return true
-  } else if (wordsInCareer.indexOf('dómari') !== -1) {
-    return true
-  }
-  return false
+function careerMatches(career, words) {
+  return words.some(word => career.indexOf(word) !== -1)
 }
 
 export function guessCareer(careerString) {
   const career = cleanString(careerString.toLowerCase())
 
-  if (isPolititian(career)) {
+  // Careers are tested in order
+  if (careerMatches(career, careerWords.politician)) {
     return 'politician'
-  } else if (isExecutive(career)) {
+  } else if (careerMatches(career, careerWords.executive)) {
     return 'executive'
-  } else if (isCivilService(career)) {
+  } else if (careerMatches(career, careerWords.civilService)) {
     return 'civil_service'
-  } else if (isWhiteCollar(career)) {
+  } else if (careerMatches(career, careerWords.whiteCollar)) {
     return 'white_collar'
-  } else if (isBarrister(career)) {
+  } else if (careerMatches(career, careerWords.barrister)) {
     return 'barrister'
-  } else if (isTeacher(career)) {
+  } else if (careerMatches(career, careerWords.teacher)) {
     return 'teacher'
-  } else if (isJournalist(career)) {
+  } else if (careerMatches(career, careerWords.journalist)) {
     return 'journalist'
-  } else if (isAthlete(career)) {
+  } else if (careerMatches(career, careerWords.athlete)) {
     return 'athlete'
-  } else if (isManualWorker(career)) {
+  } else if (careerMatches(career, careerWords.manualWorker)) {
     return 'manual_worker'
   } else {
     if (career.length !== 0) {
---
+++ career-words.js
@@ -0,0 +1,95 @@
+export const athlete = [
+  'atvinnumaður',
+]
+
+export const manualWorker = [
+  'bókari',
+  'bóndi',
+  'eftirlaunaþegi',
+  'gjaldkeri',
+  'húsmóðir',
+  'iðnrekandi',
+  'leikari',
+  'leikkona',
+  'liði',
+  'lögreglu',
+  'maður',
+  'matsveinn',
+  'meistari',
+  'prentari',
+  'sali',
+  'skógarvörður',
+  'smiður',
+  'sölufulltrúi',
+  'stuðningsfulltrúi',
+  'tækni',
+  'teiknari',
+  'verka',
+  'virki',
+  'vörður',
+  'þjálfari',
+]
+
+export const journalist = [
+  'blaðamaður',
+  'fréttamaður',
+  'útgefandi',
+  'þáttastjórnandi',
+]
+
+export const teacher = [
+  'dósent',
+  'kennari',
+  'kenndi',
+  'lektor',
+  'prófessor',
+]
+
+export const barrister = [
+  'lögmaður',
+]
+
+export const whiteCollar = [
+  'arkitekt',
+  'eftirlitsmaður',
+  'endurskoðandi',
+  'forstöðu',
+  'fræðingur',
+  'fulltrúi',
+  'heimspekingur',
+  'hjúkrun',
+  'læknir',
+  'nemi',
+  'prestur',
+  'prófastur',
+  'ráðgjafi',
+  'rithöfundur',
+  'sérfræðingur',
+  'sjúkraþjálfari',
+  'þjálfi',
+]
+
+export const civilService = [
+  'bæjarfulltrúi',
+  'borgarfulltrúi',
+  'formaður',
+  'skrifstofu',
+]
+
+export const executive = [
+  'dómari',
+  'kaupmaður',
+  'stjór',
+  'stýra',
+  'útgerðarmaður',
+]
+
+export const politician = [
+  'aðstoðarmaður',
+  'bæjarstjóri',
+  'borgarstjóri',
+  'fógeti',
+  'forseti',
+  'oddviti',
+  'sýslumaður',
+]
